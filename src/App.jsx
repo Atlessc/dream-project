2922fcd4124b6a17ce484d726888394d63ac1b0b
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-03-30 09:12:32 -0700

    slgfkj;

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,29 @@ import './App.css'
 
 function App() {
 
+  const [prompt, setPrompt] = useState('')
+  const [maxChars, setMaxChars] = useState(200)
+
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value)
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>dream</h1>
       </header>
+      <form className="prompt-container">
+        <label htmlFor="prompt"  setMaxChars={setMaxChars}>Prompt:</label>
+        <input type="textarea" id="prompt" name="prompt" value={prompt} onChange={handlePromptChange} autoFocus/>
+        {prompt.length} / {maxChars}
+        <br/>
+        <button className="Submit" type="submit">Submit</button>
+      </form>
+      <div className="image-area">image</div>
     </div>
   )
 }
