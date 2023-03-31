a12bff96930c4b06596cb178e2cfb5f2d95c97ca
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-03-31 09:34:08 -0700

    added a couple of components

---
+++ src/TextAPI.js
@@ -0,0 +1,2 @@
+// create a text completion api with the davinci model, 300 tokens, 0.9 temperature, and the prompt value coming from the input element with the ID "prompt"
+
---
+++ src/outputToggle.js
@@ -0,0 +1,2 @@
+// create a toggle a switch that switch from the text completion api and the image completion api
+
